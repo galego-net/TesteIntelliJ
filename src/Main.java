ef89e81a72733ec203f244258721f583b80261da
Galego <nuno.galego84@gmail.com>
2026-02-16 00:30:05 +0000

    sout

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("OIx");
+
+        int x =1;
+
+        //
+        System.out.println("Teste");
+
+    }
+}
